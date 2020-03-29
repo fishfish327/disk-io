4710fcb12b00f69344e34a42fb6ac397196f8b66
shuai <shuaitong963@gmail.com>
2020-03-29 09:46:23 -0500

    split rear & write

---
+++ readThroughput.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+recordSize=$1
+totalFileSize=
+num_threads=(1 2 4 8 12 24 48)
+# sequetial read test
+echo "sequential read test"
+for num_thread in ${num_threads[@]}
+do
+    filename="config${num_thread}.txt"
+    ./diskbenchmark -r -S -b $recordSize -n $num_thread -f $filename
+done
+
+
+# random read test
+echo "random read test"
+for num_thread in ${num_threads[@]}
+do 
+    filename="config${num_thread}.txt"
+    #echo $filename
+    ./diskbenchmark -r -R -b $recordSize -n $num_thread -f $filename
+done
