353485399df8c9cca5c563e9408e0ab057a155e2
Shuai Tong <stong2@hawk.iit.edu>
2020-03-29 12:43:10 +0000

    basic shell

--- testThroughput.sh
+++ testThroughput.sh
@@ -3,9 +3,8 @@
 recordSize=$1
 # sequetial read test
 num_threads=(1 2 4 8 12 24 48)
-for num_thread in {arrayIndex[@]}
-do 
-    filename="config${numthread}.txt"
-    echo $filename
-    ./diskbenchmark -r -S -b $recordSize -n num_thread -f $filename
+for num_thread in ${num_threads[@]}
+do
+    filename="config${num_thread}.txt"
+    ./diskbenchmark -r -S -b $recordSize -n $num_thread -f $filename
 done
