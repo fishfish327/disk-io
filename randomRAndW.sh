8a96a972b3c9d58f3ac05c035c267105a6f9055c
shuai <shuaitong963@gmail.com>
2020-03-29 18:53:26 -0500

    random read & write

--- randomRAndW.sh
+++ randomRAndW.sh
@@ -4,12 +4,12 @@ recordSize=$1
 totalFileSize=$2
 num_threads=(1 2 4 8 12 24 48)
 # sequetial write test
-echo "sequential write test"
+echo "random read test"
 for num_thread in ${num_threads[@]}
 do 
-    filename="config${num_thread}_write.txt"
+    filename="config${num_thread}.txt"
     echo $filename
-    ./diskbenchmark -w -S -b $recordSize -n $num_thread -f $filename -s $totalFileSize
+    ./diskbenchmark -r -R -b $recordSize -n $num_thread -f $filename -s $totalFileSize
 done
 
 # random write test
